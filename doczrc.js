79d3545791129dca93546a7585c8d414240befc4
alemesa <alejandro.suarez@jam3.com>
2019-10-01 02:45:44 -0400

    Try netlify toml

---
+++ doczrc.js
@@ -0,0 +1,34 @@
+import doczPluginNetlify from "docz-plugin-netlify";
+
+export default {
+  title: "Jam3 Hooks",
+  description: "Reusable library",
+  src: "docs/content",
+  files: "docs/content/**/*.mdx",
+  typescript: false,
+  propsParser: false,
+  codeSandbox: false,
+  htmlContext: {
+    head: {
+      links: [
+        {
+          rel: "stylesheet",
+          href: "https://codemirror.net/theme/blackboard.css"
+        }
+      ]
+    }
+  },
+  themeConfig: {
+    mode: "dark",
+    codemirrorTheme: "blackboard",
+    showPlaygroundEditor: false,
+    linesToScrollEditor: 50,
+    colors: {
+      codeColor: "#8DB6DE",
+      codeBg: "#0C1021",
+      blockquoteColor: "#8DB6DE",
+      blockquoteBg: "#0C1021"
+    }
+  },
+  plugins: [doczPluginNetlify()]
+};
